5ceae538ccba6b521a44336b702a194d68f249c4
omgshalihin <shalihinsgp@gmail.com>
2022-12-15 16:56:08 +0100

    navbar looks cleaner

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => (
-  <nav>
-    {/* <h1>All Products</h1> */}
-    <div className="logo">
+  <nav className={styles.container}>
+    <div className={styles.logo}>
       <Link className="logo" href="/">
         <Image href="/" src="/logo.png" width={60} height={60} alt="logo" />
       </Link>
     </div>
-    <Link href="/">Home</Link>
-    <Link href="/about">About</Link>
-    <Link href="/users">Members</Link>
-    <Link href="/form">Register</Link>
+    <div className={styles.links}>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/users">Members</Link>
+      <Link href="/form">Register</Link>
+    </div>
   </nav>
 );
 
