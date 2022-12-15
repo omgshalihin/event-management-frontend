487e44733500f67093e1092c6ad13ae6ce46b328
omgshalihin <shalihinsgp@gmail.com>
2022-12-15 15:57:18 +0100

    member details available now after add new member

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,56 +1,57 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
 
-export const getStaticPaths = async () => {
-  const response = await fetch('https://event-management-backend-production.up.railway.app/api/users');
-  const data = await response.json(); // data is an array of objects
-  const paths = data.map(user => ({
-    params: { id: user.id.toString() }, // return an object from this array
-  }));
-  return {
-    paths, // now we telling next.js what to build using this paths
-    fallback: false,
+
+
+const Details = () => {
+  const [member, setMember] = useState(null);
+  const router = useRouter();
+
+  const removeUser = (id) => {
+    fetch(
+      `https://event-management-backend-production.up.railway.app/api/users/${id}`,
+      {
+        method: "Delete",
+        mode: "cors",
+      }
+    );
   };
+
+  useEffect(() => {
+    const { id } = router.query;
+    
+    if (!id) return;
+
+    fetch(
+      `https://event-management-backend-production.up.railway.app/api/users/${id}`
+    )
+      .then((res) => res.json())
+      .then((data) => setMember(data))
+      .catch((error) => console.log(error));
+  }, [router]);
+
+  if (member != null) {
+    return (
+      <>
+        <div className="user">
+          <Image src={member.image} alt="avatar" width={300} height={300} />
+          <h1>{member.username}</h1>
+          <p>{member.email}</p>
+          <p>Lives in: {member.city}</p>
+          <p>From: {member.country}</p>
+        </div>
+        <Link
+          onClick={() => removeUser(member.id)}
+          href="/deleted"
+          className="btn-delete"
+        >
+          Delete Me
+        </Link>
+      </>
+    );
+  }
 };
-
-// eslint-disable-next-line max-len
-export const getStaticProps = async context => { // if there are 10 paths, this function will run 10 times
-  const { id } = context.params;
-  const response = await fetch(`https://event-management-backend-production.up.railway.app/api/users/${id}`);
-  const data = await response.json();
-  return {
-    props: { user: data },
-  };
-};
-
-const removeUser = id => {
-  fetch(`https://event-management-backend-production.up.railway.app/api/users/${id}`, {
-    method: 'Delete',
-    mode: 'cors',
-  });
-};
-
-const Details = ({ user }) => (
-  <>
-    <div className="user">
-      <Image src={user.image} alt="avatar" width={300} height={300} />
-      <h1>{user.username}</h1>
-      <p>{user.email}</p>
-      <p>
-        Lives in:
-        {' '}
-        {user.city}
-      </p>
-      <p>
-        From:
-        {' '}
-        {user.country}
-      </p>
-    </div>
-    <Link onClick={() => removeUser(user.id)} href="/deleted" className="btn-delete">Delete Me</Link>
-  </>
-
-);
 
 export default Details;
