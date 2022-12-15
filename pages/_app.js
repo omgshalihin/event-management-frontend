b5d4d7e2f65f1e9ef2d010cab6f2ce61e1b53a73
omgshalihin <shalihinsgp@gmail.com>
2022-12-15 20:19:29 +0100

    included non scalable in app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import Layout from '../components/Layout';
-import '../styles/globals.css';
+import Layout from "../components/Layout";
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
